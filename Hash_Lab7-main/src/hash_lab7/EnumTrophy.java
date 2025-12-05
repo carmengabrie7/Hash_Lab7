61d1a941fe84b2a156b42dec51833d44d2f32796
dhsib-XD <carlosgeorodriguez11@gmail.com>
2025-12-05 09:40:12 -0600

    Merge branch 'main' of https://github.com/carmengabrie7/Lab7_Hash # Please enter a commit message to explain why this merge is necessary, # especially if it merges an updated upstream into a topic branch. # # Lines starting with '#' will be ignored, and an empty message aborts # the commit.

---
+++ Hash_Lab7-main/src/hash_lab7/EnumTrophy.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hash_lab7;
+
+/**
+ *
+ * @author eduar
+ */
+
+public enum EnumTrophy {
+    PLATINO(5),ORO(3),PLATA(2),BRONCE(1);
+    
+    public int puntos;
+    
+    private EnumTrophy(int points){
+        this.puntos = puntos;
+    }
+    
+    public int getPuntos(){
+        return puntos;
+    }
+}
