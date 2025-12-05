b6566c708bfa1a17fdc2f367f4e0caddba6d05d6
carmengabrie7 <carmenelisa.gabrie7@gmail.com>
2025-12-05 10:06:37 -0600

    commitv

--- src/hash_lab7/menuGUI.java
+++ src/hash_lab7/menuGUI.java
@@ -1,13 +1,22 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package hash_lab7;
 
-/**
- *
- * @author DELL
- */
-public class menuGUI {
+import javax.swing.JFrame;
+
+
+public class menuGUI extends JFrame{
     
+    public menuGUI(){
+        setTitle ("Play Station - Menu");
+        setSize (600,600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        setLocationRelativeTo(null);
+        
+        
+    }
+    
+    public static void main (String []args){
+        menuGUI menu = new menuGUI();
+        menu.setVisible(true);
+    }
 }
